be405b1b6188de7228e7a04fc7aa88d50b8a4ddb
fahim-ahmed-7861 <fahimahmedshojib109@gmail.com>
2020-03-13 12:12:35 +0600

    done

---
+++ default.js
@@ -0,0 +1,15 @@
+function add(a,b=20)
+{
+   // if(b==undefined)
+
+ //  b= b || 0;
+
+    
+
+    return a+b;
+
+}
+
+const total=add(10,2);
+
+console.log(total);
---
+++ letconst.js
@@ -0,0 +1,8 @@
+const a=10;
+
+ const ara = [344,4435,56];
+
+ ara[1]=2333;
+  
+
+  console.log(ara[0]);
